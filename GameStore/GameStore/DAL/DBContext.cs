35736d3b13831295478985477d8009f613f82bef
DESKTOP-IA793PP\Jic <khanhlnse04056>
2017-03-22 17:28:48 +0700

    create project

---
+++ GameStore/GameStore/DAL/DBContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.DAL
+{
+    class DBContext
+    {
+        public static SqlConnection openConnection()
+        {
+            SqlConnection connection = new SqlConnection(url());
+            connection.Open();
+            return connection;
+        }
+
+        public static SqlConnection closeConnection(SqlConnection connection)
+        {
+            connection.Close();
+        }
+
+        public static string url()
+        {
+            ConfigurationManager.ConnectionStrings["connection"].ToString();
+        }
+    }
+}
